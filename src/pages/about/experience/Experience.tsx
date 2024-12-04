60f83ea071dbd6d39fdfff571855bf5319020ece
Martin Ditalo <mditalo.ext@gmail.com>
2024-12-04 14:23:45 +0800

    feat: add view resume & resume file

--- src/pages/about/experience/Experience.tsx
+++ src/pages/about/experience/Experience.tsx
@@ -92,6 +92,35 @@ const Experience = (): ReactElement => {
           </li>
         ))}
       </ol>
+      <div>
+        <a
+          className="inline-flex items-baseline font-medium leading-tight text-gray-500 hover:text-gray-800 group/link text-base"
+          href="/assets/files/Resume.pdf"
+          target="_blank"
+          rel="noreferrer noopener"
+          aria-label="View Résumé"
+        >
+          <span>
+            View&nbsp;
+            <span className="inline-block">
+              Résumé
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
+                aria-hidden="true"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </span>
+          </span>
+        </a>
+      </div>
     </div>
   );
 };
