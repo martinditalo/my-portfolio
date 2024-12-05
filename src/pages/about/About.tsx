1a12ca1a806f295c10eb044f310a138ca760f5e7
Martin Ditalo <mditalo.ext@gmail.com>
2024-12-05 10:53:34 +0800

    fix: projects alignment

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -260,7 +260,7 @@ const About = (): ReactElement => {
             <section
               ref={div2Ref}
               id="experience-container"
-              className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
+              className="mb-16 scroll-mt-16 md:mb-24 lg:mb-15 lg:scroll-mt-24"
               aria-label="Work experience"
             >
               <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-gray-700/65 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
@@ -274,7 +274,7 @@ const About = (): ReactElement => {
             <section
               ref={div3Ref}
               id="projects-container"
-              className="mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
+              className="mb-16 scroll-mt-16 md:mb-24 lg:mb-15 lg:scroll-mt-24"
             >
               <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-gray-700/65 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
                 <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">
