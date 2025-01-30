fce11bc5b798a29deb59304662149e55d632c36c
Martin Ditalo <mditalo.ext@gmail.com>
2025-01-30 11:45:40 +0800

    fix: z-index of header in mobile view

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -219,7 +219,7 @@ const About = (): ReactElement => {
               ref={div1Ref}
               className="mb-16 scroll-mt-16 md:mb-24 lg:mb-5 lg:scroll-mt-24 lg:p-3"
             >
-              <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-gray-700/65 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
+              <div className="sticky top-0 z-30 -mx-6 mb-4 w-screen bg-gray-700/65 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
                 <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">
                   About
                 </h2>
@@ -263,7 +263,7 @@ const About = (): ReactElement => {
               className="mb-16 scroll-mt-16 md:mb-24 lg:mb-15 lg:scroll-mt-24"
               aria-label="Work experience"
             >
-              <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-gray-700/65 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
+              <div className="sticky top-0 z-30 -mx-6 mb-4 w-screen bg-gray-700/65 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
                 <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">
                   Experience
                 </h2>
@@ -276,7 +276,7 @@ const About = (): ReactElement => {
               id="projects-container"
               className="mb-16 scroll-mt-16 md:mb-24 lg:mb-15 lg:scroll-mt-24"
             >
-              <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-gray-700/65 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
+              <div className="sticky top-0 z-30 -mx-6 mb-4 w-screen bg-gray-700/65 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
                 <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">
                   Projects
                 </h2>
