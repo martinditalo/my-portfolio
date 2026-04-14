7bafce591f9f39fc58b029ae17e384fa629f84f3
Martin <martinditalo@gmail.com>
2026-04-14 11:52:44 +0800

    feat: update cv

--- src/pages/about/experience/Experience.tsx
+++ src/pages/about/experience/Experience.tsx
@@ -121,7 +121,7 @@ const Experience = (): ReactElement => {
       <div className="lg:px-3">
         <a
           className="inline-flex items-baseline font-medium leading-tight text-gray-500 hover:text-gray-800 group/link text-base"
-          href="/assets/files/Ditalo_Martin_CV.pdf"
+          href="/assets/files/Ditalo_Martin-CV.pdf"
           target="_blank"
           rel="noreferrer noopener"
           aria-label="View Résumé"
